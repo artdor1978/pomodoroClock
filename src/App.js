e19ffe135dbe1ea0e76b579d09c25e53d2da92e5
Artem Dorokhov <artdor1978@gmail.com>
2019-11-08 14:08:10 +0200

    message2

--- src/App.js
+++ src/App.js
@@ -4,25 +4,39 @@ import './App.css';
 class App extends React.Component {
   render() {
     return (
-      <div className="center">
+      <div>
         <div className="leftHalf">
           <h1>Pomodoro Clock</h1>
         </div>
         <div className="rightHalf">
-          <span id="break-label">Break Length</span>
-          <span id="session-label">Session Length</span>
-          <span id="break-decrement">-</span>
-          <span id="break-length" className="slider-preview">
-            5
-          </span>
-          <span id="break-increment">+</span>
-          <span id="session-decrement">-</span>
-          <span id="session-length" className="slider-preview">
-            25
-          </span>
-          <span id="session-increment">+</span>
-          <span id="timer-label">Session</span>
-          <span id="time-left"></span>
+          <div className="flipClock">
+            <span id="timer-label">Session</span>
+            <span id="time-left"></span>
+          </div>
+          <div className="timeSlider">
+            <span id="session-label">Session Length</span>
+            <span id="session-decrement">
+              <span id="line"></span>
+            </span>
+            <span id="session-increment">
+              <span id="line"></span>
+            </span>
+            <span id="session-length" className="slider-preview">
+              25
+            </span>
+          </div>
+          <div className="timeSlider">
+            <span id="break-label">Break Length</span>
+            <span id="break-decrement">
+              <span id="line"></span>
+            </span>
+            <span id="break-increment">
+              <span id="line"></span>
+            </span>
+            <span id="break-length" className="slider-preview">
+              5
+            </span>
+          </div>
           <span id="start_stop"></span>
           <span id="reset"></span>
         </div>
